310b0cad6b6e59458df05d1822ac01d5ec5034c1
Enzo <47456460+MasterKrab@users.noreply.github.com>
2022-06-19 00:38:57 -0400

    Fix id generator

--- components/ColorSelector.tsx
+++ components/ColorSelector.tsx
@@ -31,7 +31,7 @@ const ColorSelector = () => {
           Select color
         </VisuallyHidden>
         {colors.map((color) => (
-          <ButtonColor key={Math.random()} color={color} />
+          <ButtonColor key={generateId()} color={color} />
         ))}
       </div>
       <style jsx>{`
